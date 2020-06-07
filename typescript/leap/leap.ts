d1dfae1312e99bd91b83e57d1a7744cb0a765316
Katsuki <Katsuki_Sato0130@icloud.com>
2020-06-07 20:40:01 +0900

    fix leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,10 +1,5 @@
 function isLeapYear(year: number): boolean {
-  if (year % 4 === 0) {
-    if (year % 400 === 0) return true;
-    if (year % 100 === 0) return false;
-    return true;
-  }
-  return false;
+  return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0);
 }
 
 export default isLeapYear;
