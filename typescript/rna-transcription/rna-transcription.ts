2bc2781aab15632182b96249495bf5c98257d01d
Katsuki <Katsuki_Sato0130@icloud.com>
2020-07-06 16:11:04 +0900

    Add advice

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,4 @@
+// Is a single character really a strand? I think this is just DNA and RNA.
 type DANStrand = "G" | "C" | "T" | "A";
 type RNAStrand = "C" | "G" | "A" | "U";
 
@@ -9,9 +10,11 @@ const rnaTable: { [id in DANStrand]: RNAStrand } = {
 };
 
 class Transcriptor {
+  // If you had a strand type it would describe the input and output here, would it not?
   toRna(dnaStrand: string): string {
     if (!this.isValidDnaStrand(dnaStrand)) throw "Invalid input DNA.";
 
+    // can we solve this without any arrays?
     const dnaStrandArry = [...dnaStrand] as DANStrand[];
 
     dnaStrandArry.forEach(
@@ -24,6 +27,7 @@ class Transcriptor {
   }
 
   private isValidDnaStrand(text: string): boolean {
+    // Isn't there a much simple way to just let the regex test the whole string and return a true or false result on its own?
     const matchedText = text.match(/[G|C|T|A]/g) || [];
     return text === matchedText.join("");
   }
