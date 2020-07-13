c41208123b0c556eed948d61dae206f53842de79
Katsuki <Katsuki_Sato0130@icloud.com>
2020-07-13 20:45:29 +0900

    Fix code

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,27 +1,38 @@
-// Is a single character really a strand? I think this is just DNA and RNA.
-type DANStrand = "G" | "C" | "T" | "A";
-type RNAStrand = "C" | "G" | "A" | "U";
+type DNA = "G" | "C" | "T" | "A";
+type RNA = "C" | "G" | "A" | "U";
 
-const rnaTable: { [id in DANStrand]: RNAStrand } = {
+const rnaTable: Record<DNA, RNA> = {
   G: "C",
   C: "G",
   T: "A",
   A: "U",
 };
 
+type DNAStrand = string & { _: "DNAStrand" };
+
+// function validDnaStrand(input: string) {
+//   const matchedText = input.match(/[G|C|T|A]/g) || [];
+//   if (!(input === matchedText.join(""))) throw "Invalid input DNA.";
+
+//   return input as DNAStrand;
+// }
+
 class Transcriptor {
   // If you had a strand type it would describe the input and output here, would it not?
   toRna(dnaStrand: string): string {
     if (!this.isValidDnaStrand(dnaStrand)) throw "Invalid input DNA.";
 
+    // for (let i = 0; i < dnaStrand.length; i++) {
+    //   const dna: DNA = dnaStrand.charAt(i);
+    //   dnaStrand.charAt(i) = rnaTable[dna];
+    //   // dnaStrand.replace();
+    // }
     // can we solve this without any arrays?
-    const dnaStrandArry = [...dnaStrand] as DANStrand[];
+    const dnaStrandArry = [...dnaStrand] as DNA[];
 
-    dnaStrandArry.forEach(
-      (value: DANStrand, index: number, array: string[]) => {
-        array[index] = rnaTable[value];
-      }
-    );
+    dnaStrandArry.forEach((value: DNA, index: number, array: string[]) => {
+      array[index] = rnaTable[value];
+    });
 
     return dnaStrandArry.join("");
   }
