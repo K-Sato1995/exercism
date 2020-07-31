65388992139b561cf979f41fcea42dcc78f9115f
Katsuki <Katsuki_Sato0130@icloud.com>
2020-07-31 21:05:14 +0900

    Update rna code

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,29 +1,39 @@
 type DNA = "G" | "C" | "T" | "A";
 type RNA = "C" | "G" | "A" | "U";
 
-const rnaTable: Record<DNA, RNA> = {
-  G: "C",
-  C: "G",
-  T: "A",
-  A: "U",
-};
+type DNAStrand = string;
+type RNAStrand = string;
 
 class Transcriptor {
-  toRna(dnaStrand: string) {
+  toRna(dnaStrand: DNAStrand): RNAStrand {
     if (!this.isValidDnaStrand(dnaStrand)) throw "Invalid input DNA.";
 
-    const dnaStrandArry = [...dnaStrand] as DNA[];
-
-    dnaStrandArry.forEach((value: DNA, index: number, array: string[]) => {
-      array[index] = rnaTable[value];
-    });
-
-    return dnaStrandArry.join("");
+    const rnaStrand: RNAStrand = dnaStrand.replace(
+      /(G)|(C)|(T)|(A)/g,
+      this.replacer
+    );
+    return rnaStrand;
   }
 
-  private isValidDnaStrand(text: string): boolean {
+  private isValidDnaStrand = (text: string): boolean => {
     return /^[G|C|T|A]*$/.test(text);
-  }
+  };
+
+  private replacer = (
+    _: string,
+    p1: DNA,
+    p2: DNA,
+    p3: DNA,
+    p4: DNA,
+    _offset: number,
+    _string: string
+  ) => {
+    if (p1) return "C";
+    if (p2) return "G";
+    if (p3) return "A";
+    if (p4) return "U";
+    return "";
+  };
 }
 
 export default Transcriptor;
